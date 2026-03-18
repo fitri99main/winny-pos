d083dc05fb215d8a605765a9bceb1f4fa0ca6d5f
firnandosmauna-spec <firnandosmauna@gmail.com>
2026-03-18 09:57:36 +0700

    clean android config for fresh eas build

--- mobile-app/plugins/withKotlinBypass.js
+++ mobile-app/plugins/withKotlinBypass.js
@@ -1,22 +1,18 @@
-const { withProjectBuildGradle, withGradleProperties } = require('@expo/config-plugins');
+const { withGradleProperties } = require('@expo/config-plugins');
 
 module.exports = (config) => {
-  // 1. Force Kotlin Version in build.gradle
-  config = withProjectBuildGradle(config, (config) => {
-    config.modResults.contents = config.modResults.contents.replace(
-      /kotlinVersion = findProperty\('android\.kotlinVersion'\) \?: '.*'/,
-      "kotlinVersion = '1.9.24'"
-    );
-    return config;
-  });
-
-  // 2. Add Bypass Flag in gradle.properties
+  // Suppress any leftover Kotlin/Compose version compatibility warnings
   config = withGradleProperties(config, (config) => {
-    config.modResults.push({
-      type: 'property',
-      key: 'androidx.compose.compiler.plugins.kotlin.suppressKotlinVersionCompatibilityCheck',
-      value: 'true',
-    });
+    const alreadySet = config.modResults.some(
+      (item) => item.key === 'androidx.compose.compiler.plugins.kotlin.suppressKotlinVersionCompatibilityCheck'
+    );
+    if (!alreadySet) {
+      config.modResults.push({
+        type: 'property',
+        key: 'androidx.compose.compiler.plugins.kotlin.suppressKotlinVersionCompatibilityCheck',
+        value: 'true',
+      });
+    }
     return config;
   });
 
